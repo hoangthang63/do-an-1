3037f8cc363c09deb7b1c44deda95a14a2f6eb24
hoangthang63 <anhnkno3@gmail.com>
2022-02-05 15:12:37 +0700

    validate form

--- khachhang1/signing/process_signup.php
+++ khachhang1/signing/process_signup.php
@@ -2,6 +2,7 @@
 
 $ten = $_POST['name'];
 $email = $_POST['email'];
+$gioi_tinh = $_POST['gender'];
 $mat_khau = $_POST['password'];
 
 require '../connect.php';
@@ -17,14 +18,14 @@ if($number_rows == 1){
     exit;
 }
 
-$sql = "insert into khach_hang(ten,email,mat_khau) 
-value('$ten','$email','$mat_khau') ";
+$sql = "insert into khach_hang(ten,gioi_tinh,email,mat_khau) 
+value('$ten','$gioi_tinh','$email','$mat_khau') ";
 mysqli_query($ket_noi,$sql);
 
 $sql = "select ma from khach_hang 
 where email = '$email'";
 $result = mysqli_query($ket_noi,$sql);
-$id = mysqli_fetch_array($result)['ma'];
+$id = mysqli_fetch_array($result);
 session_start();
 $_SESSION['id'] = $id;
 $_SESSION['name'] = $ten;
--- khachhang1/signing/signup.php
+++ khachhang1/signing/signup.php
@@ -5,6 +5,11 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=, initial-scale=1.0">
     <title>Document</title>
+    <style>
+        .span_loi{
+            color: red;
+        }
+    </style>
 </head>
 <body>
 <?php
@@ -18,15 +23,90 @@ session_start();
     <form method="POST" action="process_signup.php">
         <h1>Form đăng ký</h1>
         Tên
-        <input type="text" name="name">
+        <input type="text" name="name" id="ten"><span class="span_loi" id="loi_ten"></span>
+        <br>
+        Giới tính
+        <input type="radio" name="gender" value="nam">Nam
+        <input type="radio" name="gender" value="nữ">Nữ
+        <span class="span_loi" id="loi_gioi_tinh"></span>
         <br>
         Email
-        <input type="email" name="email">
+        <input type="email" name="email" id="email"><span class="span_loi" id="loi_email"></span>
         <br>
         Mật khẩu
-        <input type="password" name="password">
+        <input type="password" name="password" id="mat_khau"><span class="span_loi" id="loi_mat_khau"></span>
         <br>
-        <button>Đăng Ký</button>
+        <button type="submit" onclick="return kiem_tra()">Đăng Ký</button>
     </form>
+
+<script>
+    function kiem_tra(){
+    
+    //ten
+    let kiem_tra_loi = false;
+    let ten = document.getElementById('ten').value;
+    if(ten.length === 0)
+    {
+        document.getElementById('loi_ten').innerHTML = 'Tên không được để trống';
+        kiem_tra_loi = true;
+    }else{
+        let regex_ten = /[^a-z0-9A-Z_ÀÁÂÃÈÉÊÌÍÒÓÔÕÙÚĂĐĨŨƠàáâãèéêìíòóôõùúăđĩũơƯĂẠẢẤẦẨẪẬẮẰẲẴẶẸẺẼỀỀỂưăạảấầẩẫậắằẳẵặẹẻẽềềểỄỆỈỊỌỎỐỒỔỖỘỚỜỞỠỢỤỦỨỪễếệỉịọỏốồổỗộớờởỡợụủứừỬỮỰỲỴÝỶỸửữựỳỵỷỹ]/;
+        if(regex_ten.test(ten) == false){
+            document.getElementById('loi_ten').innerHTML = 'Tên không hợp lệ';
+            kiem_tra_loi = true;
+        }else{
+        document.getElementById('loi_ten').innerHTML = '';
+    }
+    }
+    
+    //gioi tinh
+    let mang_gioi_tinh = document.getElementsByName('gender');
+    let kiem_tra_gioi_tinh = false;
+
+    for(let i = 0; i < mang_gioi_tinh.length; i++){
+        if(mang_gioi_tinh[i].checked){
+            kiem_tra_gioi_tinh = true;
+        }
+    }
+
+    if(kiem_tra_gioi_tinh == false){
+        document.getElementById('loi_gioi_tinh').innerHTML = 'Giới tính không được để trống';
+        kiem_tra_loi = true;
+    }else{
+        document.getElementById('loi_gioi_tinh').innerHTML = '';
+    }
+    //email
+    let email = document.getElementById('email').value;
+    if(email.length === 0)
+    {
+        document.getElementById('loi_email').innerHTML = 'Email không được để trống';
+        kiem_tra_loi = true;
+    }else{
+        document.getElementById('loi_email').innerHTML = '';
+    }
+
+    //mat khau
+    let mat_khau = document.getElementById('mat_khau').value;
+    if(mat_khau.length === 0)
+    {
+        document.getElementById('loi_mat_khau').innerHTML = 'Mật khẩu không được để trống';
+        kiem_tra_loi = true;
+    }else if(mat_khau.length < 8)
+    {
+        document.getElementById('loi_mat_khau').innerHTML = 'Mật khẩu không được ít hơn 8 ký tự';
+        kiem_tra_loi = true;
+    }
+    else{
+        document.getElementById('loi_mat_khau').innerHTML = '';
+    }
+
+    if(kiem_tra_loi){
+        return false;
+    }
+
+    }
+
+
+</script>
 </body>
 </html>
